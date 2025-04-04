53c28bb0827634a654fb464221354cf08375373d
p4ddle <dewaputuadhitya@gmail.com>
2025-04-04 18:28:47 +0700

    Change users migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,11 +13,17 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
+            $table->string('phone')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->integer('province_id')->nullable();
+            $table->integer('city_id')->nullable();
+            $table->string('address')->nullable();
+            $table->integer('postcode')->nullable();
             $table->timestamps();
         });
 
