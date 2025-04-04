ed48e2c52d49502000fc4aba8bcd3bad7164b2e3
p4ddle <dewaputuadhitya@gmail.com>
2025-04-04 19:24:47 +0700

    add role users

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('first_name');
             $table->string('last_name');
+            $table->enum('role',['admin', 'user'])->default('user');
             $table->string('email')->unique();
             $table->string('phone')->unique();
             $table->timestamp('email_verified_at')->nullable();
