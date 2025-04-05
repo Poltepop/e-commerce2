36c84d16b0c0642bb9adc59b676aa0e111da40fd
Mohammad Yusfi Lakhafidun <mohammadyusfi039@gmail.com>
2025-04-05 21:32:29 +0800

    add create product service and testing product create

--- app/Livewire/Admin/ProductPage.php
+++ app/Livewire/Admin/ProductPage.php
@@ -2,11 +2,13 @@
 
 namespace App\Livewire\Admin;
 
+use App\Livewire\Forms\ProductRequest;
 use App\Models\Product;
 use Livewire\Component;
 
 class ProductPage extends Component
 {
+    public ProductRequest $productRequest;
     public function render()
     {
         $product = Product::select(['id', 'name', 'price', 'weight', 'short_description', 'description', 'status', 'created_at'])
