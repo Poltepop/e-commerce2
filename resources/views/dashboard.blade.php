b34f2d17f3ae52f662caf154cbb4fd49940d6a32
p4ddle <dewaputuadhitya@gmail.com>
2025-04-05 20:48:55 +0700

    dashboard view

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,53 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
+        <x-layout.admin.header>
+            Dashboard
+        </x-layout.admin.header>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+    <div class="py-5">
+        <div class="max-w-7xl mx-4 sm:px-6 md:mx-auto  lg:px-8">
+            <x-layout.admin.card class=" text-black flex justify-between items-center">
+                {{-- Avatar --}}
+                <div class="flex">
+                    <div class="avatar">
+                        <div class="w-14 rounded-full">
+                            <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                        </div>
+                    </div>
+                    
+                    <div class="flex flex-col ml-4">
+                        <h1 class="font-extrabold text-lg">Welcome</h1>
+                        <h2 class="font-thin">{{ Auth::user()->first_name }} {{ Auth::user()->last_name }}</h2>
+                    </div>
                 </div>
+
+                <form action="{{ route('logout') }}" method="post">
+                    @csrf
+                    <x-normal-button class="rounded-sm w-36 hidden md:flex">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5 text-gray-700">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" />
+                        </svg>
+                        
+                        <h5>Sign Out</h5>
+                    </x-normal-button>
+                </form>
+
+            </x-layout.admin.card>
+
+            <div class="flex mt-5">
+                <x-layout.admin.card class="w-full text-black mr-5 flex-col">
+                    <h1>Lorem ipsum dolor sit, amet consectetur adipisicing.</h1>
+                    <h2 class="font-extrabold text-4xl">700</h2>
+                </x-layout.admin.card>
+                <x-layout.admin.card class="w-full text-black flex-col">
+                    <h1 class="">Lorem ipsum dolor sit amet consectetur adipisicing.</h1>
+                    <h2 class=" font-extrabold text-4xl">12</h2>
+                </x-layout.admin.card>
             </div>
+            <x-layout.admin.card class="w-full text-black h-80 mt-5 justify-center items-center">
+                Content
+            </x-layout.admin.card>
         </div>
     </div>
 </x-app-layout>
