ef70da9d999001e21466680504d6ad7a8e60e92a
p4ddle <dewaputuadhitya@gmail.com>
2025-04-05 10:30:42 +0700

    view login,register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,8 +2,29 @@
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
+    <div class="text-center mb-10">
+        <h1 class=" font-extrabold text-xl mb-3 font-poppins">Login</h1>
+        <p class="font-poppins">Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
+    </div>
+
     <form method="POST" action="{{ route('login') }}">
         @csrf
+
+        <div class="flex flex-col items-center space-y-4">
+            <!-- Tombol Google -->
+            <x-normal-button disabeld>
+                <img src="https://www.svgrepo.com/show/475656/google-color.svg" alt="Google Logo" class="w-5 h-5 mr-2">
+                <span>Login with Google</span>
+            </x-normal-button>
+          
+            <!-- Tombol Facebook -->   
+            <x-normal-button disabeld>
+                <img src="https://www.svgrepo.com/show/475647/facebook-color.svg" alt="Facebook Logo" class="w-5 h-5 mr-2">
+                <span>Login with Facebook</span>
+            </x-normal-button>
+        </div>
+
+        <div class="divider text-gray-400 my-10">or Signin with Email</div>
 
         <!-- Email Address -->
         <div>
@@ -25,23 +46,24 @@
         </div>
 
         <!-- Remember Me -->
-        <div class="block mt-4">
+        <div class="flex mt-4 justify-between">
             <label for="remember_me" class="inline-flex items-center">
                 <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
                 <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
             </label>
-        </div>
 
-        <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
+        </div>
 
-            <x-primary-button class="ms-3">
+        <div class="flex items-center justify-end mt-6">
+
+            <x-normal-button class=" bg-blue-950 text-white font-poppins font-bold">
                 {{ __('Log in') }}
-            </x-primary-button>
+            </x-normal-button>
         </div>
     </form>
 </x-guest-layout>
