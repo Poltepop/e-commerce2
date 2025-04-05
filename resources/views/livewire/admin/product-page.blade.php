f714ea4f07e6810a5eda5cb74868e43632c468bd
Mohammad Yusfi Lakhafidun <mohammadyusfi039@gmail.com>
2025-04-05 12:30:13 +0800

    set up product page

---
+++ resources/views/livewire/admin/product-page.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <h1>Hello Product</h1>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Middleware\OnlyAdminMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +17,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware([OnlyAdminMiddleware::class, 'auth'])
+        ->controller(ProductController::class)->group(function () {
+    Route::get('/admin/products', 'adminProduct')->name('admin.products');
 });
 
 require __DIR__.'/auth.php';
