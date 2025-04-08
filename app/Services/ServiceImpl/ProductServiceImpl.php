dd91d4836292f2e0f5b5512a68f417fb86f3fa28
Mohammad Yusfi Lakhafidun <mohammadyusfi039@gmail.com>
2025-04-08 10:52:10 +0800

    update GenerateSlug and implementasi on service

--- app/Services/ServiceImpl/ProductServiceImpl.php
+++ app/Services/ServiceImpl/ProductServiceImpl.php
@@ -5,25 +5,31 @@ namespace App\Services\ServiceImpl;
 use App\Services\ProductService;
 use App\Livewire\Forms\ProductRequest;
 use App\Models\Product;
+use App\Utils\GenerateSlug;
 
 class ProductServiceImpl implements ProductService {
+    use GenerateSlug;
     public function create(Product $product): void {
         $product->status = empty(trim($product->status)) ? 'new' : $product->status;
+        $product->slug = $this->generateProductSlug($product->name)->getSlug();
         $product->save();
     }
 
     public function update(int $productId, Product $product): void
     {
         $productUpdate = Product::find($productId);
-        $productUpdate?->update([
-            'name' => $product->name,
-            'slug' => $product->slug,
-            'price' => $product->price,
-            'weight' => $product->weight,
-            'short_description' => $product->short_description,
-            'description' => $product->description,
-            'status' => $product->status,
-        ]);
+
+        if($productUpdate->exists())
+            $slug = $this->generateProductSlug($product->name)->getSlug();
+            $productUpdate?->update([
+                'name' => $product->name,
+                'slug' => $slug,
+                'price' => $product->price,
+                'weight' => $product->weight,
+                'short_description' => $product->short_description,
+                'description' => $product->description,
+                'status' => $product->status,
+            ]);
     }
 
     public function delete(int $productId): void
