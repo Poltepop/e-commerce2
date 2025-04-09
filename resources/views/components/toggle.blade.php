cf80d0568cd620c0de5bdd56ff237d6c4b0b1700
Mohammad Yusfi Lakhafidun <mohammadyusfi039@gmail.com>
2025-04-09 14:31:42 +0800

    fix bug  taggle button #1

--- resources/views/components/toggle.blade.php
+++ resources/views/components/toggle.blade.php
@@ -1,7 +1,21 @@
 @props(['color' => 'blue'])
 
-<label class="relative inline-flex items-center cursor-pointer">
-    <input type="checkbox" class="sr-only peer" {{ $attributes->merge([]) }}>
-    <div class="w-11 h-6 bg-gray-300 peer-focus:outline-none peer-focus:ring-1 peer-focus:ring-gray-300 rounded-full peer peer-checked:bg-{{$color}}-500 transition-colors"></div>
-    <div class="absolute left-1 top-1 bg-white w-4 h-4 rounded-full transition-transform peer-checked:translate-x-5"></div>
+<label x-data="{ checked: false }" class="relative inline-flex items-center cursor-pointer">
+    <input
+        type="checkbox"
+        x-model="checked"
+        class="sr-only peer"
+        {{ $attributes->merge([]) }}>
+
+    <div
+        :class="checked
+            ? 'bg-orange-500'
+            : 'bg-gray-300'"
+        class="w-11 h-6 rounded-full transition-colors peer-focus:outline-none peer-focus:ring-1 peer-focus:ring-gray-300">
+    </div>
+
+    <div class="absolute left-1 top-1 bg-white w-4 h-4 rounded-full transition-transform"
+        :class="checked ? 'translate-x-5' : ''">
+    </div>
 </label>
+
