9988808e904decb075562da317e71e6bf6634673
Mohammad Yusfi Lakhafidun <mohammadyusfi039@gmail.com>
2025-04-13 15:05:17 +0800

    ADD NEW LAYOUT FOR USER

--- app/Livewire/User/Home.php
+++ app/Livewire/User/Home.php
@@ -2,10 +2,12 @@
 
 namespace App\Livewire\User;
 
+use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 class Home extends Component
 {
+    #[Layout('layouts.user.app-layout')]
     public function render()
     {
         return view('livewire.user.home');
