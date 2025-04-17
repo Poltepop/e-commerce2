d2dcc3dcd2ede069879e4d49e47af44309b89bde
p4ddle <dewaputuadhitya@gmail.com>
2025-04-17 11:42:20 +0700

    Component: card variant

---
+++ resources/views/components/card-variant-four.blade.php
@@ -0,0 +1,31 @@
+<div class="p-5 flex rounded-3xl bg-white/60">
+
+    <div class="mr-5"> 
+        <img src="storage/img/card.jpg" alt="" class=" rounded-3xl min-w-30 min-h-44 max-w-30 max-h-40">
+    </div>
+
+    <div class="text-black flex flex-col gap-3">
+        <h1 class="font-bold text-xs mb-10">New Arrivals</h1>
+        <p class="font-bold text-xs">Borem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <div class="rating rating-xs">
+            <input type="radio" name="rating-5" class="mask mask-star-2 bg-orange-400" />
+            <input
+              type="radio"
+              name="rating-5"
+              class="mask mask-star-2 bg-orange-400"
+              checked="checked" />
+              <input type="radio" name="rating-5" class="mask mask-star-2 bg-orange-400" />
+              <input type="radio" name="rating-5" class="mask mask-star-2 bg-orange-400" />
+              <input type="radio" name="rating-5" class="mask mask-star-2 bg-orange-400" />
+        </div>
+        <span class="font-bold text-sm">Rp.450.000</span>
+    </div>
+
+    <div class="items-end text-black ml-auto">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+        </svg>
+          
+    </div>
+
+</div>
