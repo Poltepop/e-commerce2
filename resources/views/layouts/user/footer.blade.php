38602ef8ccd6aca88e559373c87562f886717299
p4ddle <dewaputuadhitya@gmail.com>
2025-04-23 10:04:14 +0700

    footer

---
+++ resources/views/layouts/user/footer.blade.php
@@ -0,0 +1,83 @@
+
+<div class="p-10 bg-gray-950 text-white">
+    <div class="flex justify-between items-center">
+
+        <div class="flex gap-16  w-full items-center">
+            <h1 class="font-bold text-xl text-nowrap">Sign Up Newsletter</h1>
+
+            <label for="" class="border p-1 h-10 w-full max-w-3xl flex justify-between rounded-lg">
+                <input type="text" class=" w-full h-7 bg-transparent border-none focus:outline-none focus:ring-0 placeholder:text-white" placeholder="Your Email Addres">
+                <button class="bg-white text-black text-sm px-7 py-1 font-bold rounded-lg">Send</button>
+            </label>
+        </div>
+
+        <div class="flex flex-col w-full max-w-xs pl-10">
+            <h1 class="text-xl">Follow us on: </h1>
+            <div class="flex">
+                {{-- Logos --}}
+            </div>
+        </div>
+
+
+    </div>
+
+    <div class="flex justify-evenly gap-20 mt-16">
+        <div class="flex flex-col">
+            <h1 class="font-bold text[17px] mb-3">Privacy And Policy</h1>
+            <span class="text-[16px]">Return & Exchanges</span>
+            <span class="text-[16px]">Paymeny Terms</span>
+            <span class="text-[16px]">Delivery Terms</span>
+            <span class="text-[16px]">Payment & pricing</span>
+            <span class="text-[16px]">Terms Of use</span>
+            <span class="text-[16px]">Privacy Policiy</span>
+        </div>
+        <div class="flex flex-col">
+            <h1 class="font-bold text[17px] mb-3">Privacy And Policy</h1>
+            <span class="text-[16px]">Return & Exchanges</span>
+            <span class="text-[16px]">Paymeny Terms</span>
+            <span class="text-[16px]">Delivery Terms</span>
+            <span class="text-[16px]">Payment & pricing</span>
+            <span class="text-[16px]">Terms Of use</span>
+            <span class="text-[16px]">Privacy Policiy</span>
+        </div>
+        <div class="flex flex-col">
+            <h1 class="font-bold text[17px] mb-3">Privacy And Policy</h1>
+            <span class="text-[16px]">Return & Exchanges</span>
+            <span class="text-[16px]">Paymeny Terms</span>
+            <span class="text-[16px]">Delivery Terms</span>
+            <span class="text-[16px]">Payment & pricing</span>
+            <span class="text-[16px]">Terms Of use</span>
+            <span class="text-[16px]">Privacy Policiy</span>
+        </div>
+        <div class="flex flex-col">
+            <h1 class="font-bold text[17px] mb-3">Privacy And Policy</h1>
+            <span class="text-[16px]">Return & Exchanges</span>
+            <span class="text-[16px]">Paymeny Terms</span>
+            <span class="text-[16px]">Delivery Terms</span>
+            <span class="text-[16px]">Payment & pricing</span>
+            <span class="text-[16px]">Terms Of use</span>
+            <span class="text-[16px]">Privacy Policiy</span>
+        </div>
+    </div>
+
+    <div class="flex justify-between mt-16 mr-40">
+        <h1 class="font-bold text-[20px]">BRND</h1>
+
+        <div class="text-center">
+            <h1 class="font-bold">Call Center</h1>
+            <span>+001 2345 678</span>
+        </div>
+
+        <div class="text-center">
+            <h1 class="font-bold">Free Delivery</h1>
+            <span>For all amounts over $100</span>
+        </div>
+
+        <div class="text-center">
+            <h1 class="font-bold">Gift Cards</h1>
+            <span>Save up to 20%</span>
+        </div>
+
+    </div>
+
+</div>
--- resources/views/livewire/user/home.blade.php
+++ resources/views/livewire/user/home.blade.php
@@ -114,67 +114,6 @@
         </div>
     </div>
 
-    <div class="p-10 bg-gray-950 text-white">
-        <div class="flex justify-between items-center">
-
-            <div class="flex gap-16  w-full items-center">
-                <h1 class="font-bold text-xl text-nowrap">Sign Up Newsletter</h1>
-
-                <label for="" class="border p-1 h-10 w-full max-w-3xl flex justify-between rounded-lg">
-                    <input type="text" class=" w-full h-7 bg-transparent border-none focus:outline-none focus:ring-0 placeholder:text-white" placeholder="Your Email Addres">
-                    <button class="bg-white text-black text-sm px-7 py-1 font-bold rounded-lg">Send</button>
-                </label>
-            </div>
-
-            <div class="flex flex-col w-full max-w-xs pl-10">
-                <h1 class="text-xl">Follow us on: </h1>
-                <div class="flex">
-                    {{-- Logos --}}
-                </div>
-            </div>
-
-
-        </div>
-
-        <div class="flex justify-evenly gap-20 mt-16">
-            <div class="flex flex-col">
-                <h1 class="font-bold text[17px] mb-3">Privacy And Policy</h1>
-                <span class="text-[16px]">Return & Exchanges</span>
-                <span class="text-[16px]">Paymeny Terms</span>
-                <span class="text-[16px]">Delivery Terms</span>
-                <span class="text-[16px]">Payment & pricing</span>
-                <span class="text-[16px]">Terms Of use</span>
-                <span class="text-[16px]">Privacy Policiy</span>
-            </div>
-            <div class="flex flex-col">
-                <h1 class="font-bold text[17px] mb-3">Privacy And Policy</h1>
-                <span class="text-[16px]">Return & Exchanges</span>
-                <span class="text-[16px]">Paymeny Terms</span>
-                <span class="text-[16px]">Delivery Terms</span>
-                <span class="text-[16px]">Payment & pricing</span>
-                <span class="text-[16px]">Terms Of use</span>
-                <span class="text-[16px]">Privacy Policiy</span>
-            </div>
-            <div class="flex flex-col">
-                <h1 class="font-bold text[17px] mb-3">Privacy And Policy</h1>
-                <span class="text-[16px]">Return & Exchanges</span>
-                <span class="text-[16px]">Paymeny Terms</span>
-                <span class="text-[16px]">Delivery Terms</span>
-                <span class="text-[16px]">Payment & pricing</span>
-                <span class="text-[16px]">Terms Of use</span>
-                <span class="text-[16px]">Privacy Policiy</span>
-            </div>
-            <div class="flex flex-col">
-                <h1 class="font-bold text[17px] mb-3">Privacy And Policy</h1>
-                <span class="text-[16px]">Return & Exchanges</span>
-                <span class="text-[16px]">Paymeny Terms</span>
-                <span class="text-[16px]">Delivery Terms</span>
-                <span class="text-[16px]">Payment & pricing</span>
-                <span class="text-[16px]">Terms Of use</span>
-                <span class="text-[16px]">Privacy Policiy</span>
-            </div>
-        </div>
-
-    </div>
+    <x-user.footer></x-user.footer>
 
 </div>
